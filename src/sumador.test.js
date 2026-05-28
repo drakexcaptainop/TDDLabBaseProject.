bd0a81b98fc8f87896bcaa324eb1fc338e061bbe
cazadorDeColombianos <90215784+drakexcaptainop@users.noreply.github.com>
2026-05-27 20:54:35 -0400

    simple sum

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,9 +1,11 @@
 import sumar from "./sumador.js";
 
-describe("Sumar", () => {
-  it("deberia sumar dos numeros", () => {
-    expect(sumar(3, 2)).toEqual(5);
+describe("Calculadora de cadenas", () => {
+  it("debería retornar 0 para una cadena vacía", () => {
+    expect(sumar("")).toEqual(0);
   });
 });
+
+
 
 
