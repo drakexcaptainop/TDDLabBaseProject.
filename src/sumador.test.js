a799556628e2f24e137cc940020d7c1be57eabfb
cazadorDeColombianos <90215784+drakexcaptainop@users.noreply.github.com>
2026-05-27 20:56:53 -0400

    single number sum

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -4,6 +4,10 @@ describe("Calculadora de cadenas", () => {
   it("debería retornar 0 para una cadena vacía", () => {
     expect(sumar("")).toEqual(0);
   });
+
+  it("debería retornar el número para una cadena con un solo número", () => {
+    expect(sumar("2")).toEqual(2);
+  });
 });
 
 
